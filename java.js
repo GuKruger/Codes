1d93a488aaf0aaa74a8a1483a5f14ce32fdc6b07
GuKruger <gukruger.laporte@gmail.com>
2023-02-02 18:11:56 -0500

    Something

---
+++ java.js
@@ -0,0 +1,9 @@
+var firstName = "Gustavo ";
+
+var lastName = "Kruger";
+
+var age = 18;
+
+alert("My name is " + firstName + lastName);
+alert("I am " + age + " years old");
+console.log("I am actually loving this class :)");
